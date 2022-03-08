505d34d29a9632974fec634616df7302be44e37c
evan <evan.chang@sinyentech.com>
2022-03-08 21:24:15 +0800

    add pinia template

---
+++ .vscode/blueprint-templates/pinia-template/__name__.js
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+
+export const use{{pascalCase name}}Store  = defineStore('{{name}}', {
+    state: () => {
+        return {
+        
+        }
+    },
+    getters: {
+
+    },
+    actions: {
+
+    },
+})
